690dbfa271c90e9376ac4c5a89f4b79786bf0f38
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-08 23:59:37 -0300

    :wrench: style: Alterar width do input

--- src/components/header/Input.tsx
+++ src/components/header/Input.tsx
@@ -1,5 +1,5 @@
 export function Input() {
     return (
-        <input type="text" className="w-full min-w-[180px] h-[56.16px] mr-4 border-solid border-2 rounded-md border-[#454D4A] px-3 font-semibold placeholder-[#DBDADA] focus:outline-none" placeholder="Procure seu item" />
+        <input type="text" className="min-w-[964.67px] h-[56.16px] mr-4 rounded-md  border-solid border-2 border-[#454D4A] px-3 font-semibold placeholder-[#DBDADA] focus:outline-none" placeholder="Procure seu item" />
     );
 }
